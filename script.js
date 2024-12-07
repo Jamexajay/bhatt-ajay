11c10962d7aae2e370eb7c8536e6c6b96a160681
Jamexajay <154242589+Jamexajay@users.noreply.github.com>
2024-12-07 19:49:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  function agecalculatorfunc () {
+    window.location.href = 'agecal.html'; // Replace 'another.html' with your target file's name
+  }
+
+  function calfunc() {
+    window.location.href = 'cal.html'; // Replace 'another.html' with your target file's name
+  }
